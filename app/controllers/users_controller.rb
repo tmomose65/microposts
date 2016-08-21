bfc886616e70eb67a28762e1e97b653b8c83ce3f
tmomose <t.momose65@gmail.com>
2016-08-21 14:34:36 +0000

    syuusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
   end
   
   def followings
-    @user = current_user
+    @user = User.find(params[:id])
     @followings = @user.following_users
   end
   
   def followers
-    @user = current_user
+    @user = User.find(params[:id])
     @followers = @user.follower_users
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get    'signup', to: 'users#new'
-  get    'followings', to: 'users#followings'
-  get    'followers', to: 'users#followers'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+
 
   resources :users
   resources :microposts
